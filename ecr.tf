61996bfc2ab2cb8ea46a59f64958b23ab906f04a
Matteo <santiagobernabeujr@gmail.com>
2026-04-02 11:50:53 +0200

    Configure docker build and push to ecr

---
+++ ecr.tf
@@ -0,0 +1,22 @@
+resource "aws_ecr_repository" "app" {
+  name = "my-app-repo"
+}
+
+resource "aws_ecr_lifecycle_policy" "app_policy" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 5 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 5
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output "sns_topic_arn" {
 output "cloudtrail_bucket_name" {
   value = aws_s3_bucket.cloudtrail_logs.id
 }
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
